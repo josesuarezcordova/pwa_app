816bf3046a1882823af56faf8ef36e181ad88627
Jose <demo@demo.com>
2025-10-14 22:10:05 +0100

    Remove service worker duplicated

--- src/index.js
+++ src/index.js
@@ -10,22 +10,32 @@ const isLocalhost = ['localhost', '127.0.0.1'].includes(window.location.hostname
 console.log('Enviroment loaded :', process.env);
 
 //In development , ensure no SW is intercepting requests
-if('serviceWorker' in navigator && isLocalhost) {
-  navigator.serviceWorker.getRegistrations().then((regs) => regs.forEach((r) => r.unregister()));
-  console.log('Unregistered existing service workers on localhost for development purposes.');
-}
+// if('serviceWorker' in navigator && isLocalhost) {
+//   navigator.serviceWorker.getRegistrations().then((regs) => regs.forEach((r) => r.unregister()));
+//   console.log('Unregistered existing service workers on localhost for development purposes.');
+// }
 
 // In production, register the service worker using a relative URL
-if('serviceWorker' in navigator && !isLocalhost) {
-  const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-  navigator.serviceWorker
-  .register(swUrl)
-  .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-  })
-  .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-  }); 
+// if('serviceWorker' in navigator && !isLocalhost) {
+//   const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+//   navigator.serviceWorker
+//   .register(swUrl)
+//   .then((registration) => {
+//       console.log('Service Worker registered with scope:', registration.scope);
+//   })
+//   .catch((error) => {
+//       console.error('Service Worker registration failed:', error);
+//   }); 
+// }
+
+// Register SW only in production
+if (process.env.NODE_ENV === 'production') {
+  serviceWorkerRegistration.register();
+} else {
+  // In dev, ensure no SW interferes
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
+  }
 }
 
 const basename = process.env.PUBLIC_URL || '/';
